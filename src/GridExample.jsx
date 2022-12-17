e75e2cd091a7c106e4174efd65d3b90af4833903
Tetyana123 <oookbtatyana@gmail.com>
2022-12-17 19:51:55 +0200

    Some message

--- src/GridExample.jsx
+++ src/GridExample.jsx
@@ -25,7 +25,7 @@ function GridExample() {
               <Card.Text>
                 This is a longer card with supporting text below as a natural
                 lead-in to additional content. This content is a little bit
-                longer.
+                longer.......
               </Card.Text>
             </Card.Body>
           </Card>
